41d111e601af0f5c2f893139975f389776c7ca3d
Wayne Redmond <wredmon2@icloud.com>
2025-05-17 09:42:08 +0200

    Add project files.

---
+++ InterviewTests/PracticeInterview.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+
+public class HelloWorld
+{
+    static public void Main()
+    {
+        String line;
+        String A;
+        A = Console.ReadLine();
+        String P;
+        P = Console.ReadLine();
+
+        int out_ = count_like_dislike(A, P);
+        Console.Out.WriteLine(out_);
+    }
+
+    static int count_like_dislike(String A, String P)
+    {
+        int count = 0;
+        for (int i = 0; i < A.Length; i++)
+        {
+            if (A[i] == P[i])
+            { 
+                count++;
+            }
+        }
+        return count;
+
+    }
+
+}
---
+++ InterviewTests/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Linq;
+
+//To make factorials
+public class FactorialProgram
+{
+    public static void Main()
+    {
+        int output = 1;
+        int N = int.Parse(Console.ReadLine());
+        for (int i = N; i > 0; i--)
+        {
+            output = output * i;
+        }
+        Console.WriteLine(output.ToString());
+    }
+}
+
+//Basic input and output
+//uing System;
+public class IOProgram
+{
+    public static void Main()
+    {
+        int In = int.Parse(Console.ReadLine());
+        string Input = Console.ReadLine();
+        In = In * 2;
+        Console.WriteLine(In.ToString());
+        Console.WriteLine(Input.ToString());
+    }
+}
+//Favorite Singer Reading and counting arrays
+//using System;
+public class FavoriteSinger
+{
+    public static void Main()
+    {
+        long N = long.Parse(Console.ReadLine());
+        long[] Songs = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+        long Singer = 0;
+        long Counter = 0;
+        for (int i = 0; i < N; i++)
+        {
+            bool alreadyCounted = false;
+            for (int k = 0; k < i; k++)
+            {
+                if (Songs[k] == Songs[i])
+                {
+                    alreadyCounted = true;
+                    break;
+                }
+            }
+            if (!alreadyCounted)
+            {
+                long count = 0;
+                for (int j = 0; j < N; j++)
+                {
+                    if (Songs[j] == Songs[i])
+                        count++;
+                }
+
+                if (count > Counter)
+                {
+                    Counter = count;
+                    Singer = 1;
+                }
+                else if (count == Counter)
+                {
+                    Singer++;
+                }
+            }
+        }
+        Console.WriteLine(Singer.ToString());
+    }
+}
+
+//ZOOO
+//using System;
+//using System.Linq;
+public class ZOOO
+{
+    public static void Main()
+    {
+        string Input = Console.ReadLine().ToUpper();
+        int Z = 0;
+        int O = 0;
+        Z = Input.Count(c => c == 'Z');
+        O = Input.Count(c => c == 'O');
+        
+        if (2 * Z == O)
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
+
+//Divisibility
+//using System;
+public class Divisability
+{
+    public static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        int[] Numbers = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        string Output= "";
+        for (int i = 0; i < N; i++)
+        {
+            string Number = Numbers[i].ToString();
+            Output = Output + Number.Last();
+        }
+        if (Output.Last() == '0')
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+
+}
+
+
